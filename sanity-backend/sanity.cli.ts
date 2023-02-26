bbcd276b489d50a8b33eab907864cb3cd82f49f1
Antonio Lopez <pezlop202@gmail.com>
2023-02-25 21:35:01 -0800

    Add sanity/cli

--- sanity-backend/sanity.cli.ts
+++ sanity-backend/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import {defineCliConfig} from '@sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'jkn6q794',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
