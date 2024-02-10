4109f2fb839c5d79dc3bd5c72301f6b4633c2f5a
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2024-02-10 12:32:08 -0800

    Update seo twitter props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -27,6 +28,18 @@ function MyApp({ Component, pageProps }: AppProps) {
           ],
         }}
       />
+      <Head>
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='Sk8 N Meet Santana' />
+        <meta
+          name='twitter:image'
+          content='https://res.cloudinary.com/dzhengjen/image/upload/v1707594527/assets/logos/og-sk8nmeet_px9f4p.png'
+        />
+        <meta
+          name='twitter:description'
+          content='The first non-traditional skateboarding group in Santa Ana who hosts monthly meet ups and aim to provide a thriving safe space for skaters who identify as female, queer, trans and/or nonbinary.'
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
