91daa9d17d63dc73ab4e84251deec45c8ec3a9b3
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2024-02-10 11:57:46 -0800

    Update seo open graph props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,28 @@
-import '../styles/globals.css';
-import { NextSeo } from 'next-seo';
-import type { AppProps } from 'next/app';
-import Layout from '../components/Layout/Layout';
+import "../styles/globals.css";
+import { NextSeo } from "next-seo";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <NextSeo
-        title='Sk8NMeet Santana'
-        description='The first non-traditional skateboarding group in Santa Ana who hosts monthly meet ups for all skill levels by creating community, friendship, and lasting connections through skateboarding. We aim to provide thriving safe spaces for skaters who identify as female, queer, trans and/or nonbinary.'
+        title='Sk8 N Meet Santana'
+        description='The first non-traditional skateboarding group in Santa Ana who hosts monthly meet ups and aim to provide a thriving safe space for skaters who identify as female, queer, trans and/or nonbinary.'
         canonical='https://sk8nmeetsantana.com/'
         openGraph={{
-          type: 'website',
-          url: 'https://sk8nmeetsantana.com/',
-          title: 'Sk8NMeet Santana',
+          type: "website",
+          url: "https://sk8nmeetsantana.com/",
+          title: "Sk8 N Meet Santana",
           description:
-            'The first non-traditional skateboarding group in Santa Ana who hosts monthly meet ups for all skill levels by creating community, friendship, and lasting connections through skateboarding. We aim to provide thriving safe spaces for skaters who identify as female, queer, trans and/or nonbinary.',
+            "The first non-traditional skateboarding group in Santa Ana who hosts monthly meet ups and aim to provide a thriving safe space for skaters who identify as female, queer, trans and/or nonbinary.",
           images: [
             {
-              url: 'https://res.cloudinary.com/dloisor1x/image/upload/c_scale,w_1200/v1672271664/santa-ana-sk8nmeet/logos/Sk8_n_meet-_Santa_Ana_logo_ot8d2y.png',
+              url: "https://res.cloudinary.com/dzhengjen/image/upload/v1707594527/assets/logos/og-sk8nmeet_px9f4p.png",
               width: 1200,
-              height: 675,
-              alt: 'Sk8NMeet logo',
-              type: 'image/png',
+              height: 630,
+              alt: "Sk8 N Meet logo",
+              type: "image/png",
             },
           ],
         }}
